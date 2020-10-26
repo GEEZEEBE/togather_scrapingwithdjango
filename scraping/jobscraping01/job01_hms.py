d4045f4ddd5bc187fcb4d24e3e799c052b88f007
moon <aa6271235@naver.com>
2020-10-26 17:59:51 +0900

    moon

--- scraping/jobscraping01/job01_hms.py
+++ scraping/jobscraping01/job01_hms.py
@@ -1,4 +1,4 @@
-import bs4, time, selenium, requests, json, schedule
+import bs4, time, selenium, requests, json, schedule, os
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
@@ -6,14 +6,26 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from pymongo import MongoClient
+from selenium.webdriver.chrome.options import Options
 
-driver = webdriver.Chrome(executable_path='/home/rapa/Documents/Develops/chromedriver')
+
 db_url='mongodb://127.0.0.1:27017'
 
 def jobprocess():
+
+    # 드라이버 불러오기
+    strfile = os.path.dirname(os.path.realpath(__file__))
+    strfile = strfile + "/chromedriver_86_4240"
+
+    options = Options()
+    options.headless = True
+    driver = webdriver.Chrome(executable_path=strfile, options=options)
+    driver.implicitly_wait(3)
+
     now = time.strftime('%Y-%m-%d', time.localtime(time.time()))
     Insert_name = '황문섭'
     driver.get('https://www.work.go.kr/empInfo/empInfoSrch/list/dtlEmpSrchList.do?careerTo=&keywordJobCd=&occupation=133100%2C133101%2C133200%2C134101%2C134102&rot2WorkYn=&templateInfo=&payGbn=&resultCnt=10&keywordJobCont=&cert=&cloDateStdt=&moreCon=more&minPay=&codeDepth2Info=11000&isChkLocCall=&sortFieldInfo=DATE&major=&resrDutyExcYn=&sortField=DATE&staArea=&sortOrderBy=DESC&keyword=&termSearchGbn=all&benefitSrchAndOr=O&disableEmpHopeGbn=&webIsOut=&actServExcYn=&keywordStaAreaNm=&maxPay=&emailApplyYn=&listCookieInfo=DTL&pageCode=&codeDepth1Info=11000&keywordEtcYn=&publDutyExcYn=&keywordJobCdSeqNo=&exJobsCd=&templateDepthNmInfo=&computerPreferential=&regDateStdt=&employGbn=&empTpGbcd=1&region=&resultCntInfo=10&siteClcd=all&cloDateEndt=&sortOrderByInfo=DESC&currntPageNo=1&indArea=&careerTypes=N&searchOn=Y&subEmpHopeYn=&academicGbn=04&foriegn=&templateDepthNoInfo=&mealOfferClcd=&station=&moerButtonYn=&holidayGbn=&enterPriseGbn=all&academicGbnoEdu=&cloTermSearchGbn=all&keywordWantedTitle=&stationNm=&benefitGbn=&keywordFlag=&essCertChk=&isEmptyHeader=&depth2SelCode=&_csrf=5c5ab007-f9f9-4c7c-88fe-f811bf6c31f3&keywordBusiNm=&preferentialGbn=all&rot3WorkYn=&pfMatterPreferential=&regDateEndt=&staAreaLineInfo1=11000&staAreaLineInfo2=1&pageIndex=1&termContractMmcnt=&careerFrom=&laborHrShortYn=#viewSPL')
+    
     for i in range(1, 10):
         C_date = driver.find_element_by_xpath(f'//*[@id="list{i}"]/td[4]/div/p[3]').text
         C_name = driver.find_element_by_xpath(f'//*[@id="list{i}"]/td[2]/a').text
@@ -28,6 +40,7 @@ def jobprocess():
 
         driver.implicitly_wait(10)
 
+        with MongoClient(db_url) as client:
             job = client['jobdb']
             try:
                 jobs = driver.find_elements_by_xpath('//*[@id="contents"]/div/div/div')
